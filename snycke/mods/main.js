c3319ee46a63108dc41307b894f8fb9c054147ae
Simon-Ybarra <sybarra6@calstatela.edu>
2024-11-15 20:53:46 -0800

    Update main.js
    Overhauled onload to model after melonjs platformer example with adjustments for our assets

--- snycke/mods/main.js
+++ snycke/mods/main.js
@@ -1,6 +1,52 @@
 import * as me from 'melonjs';
-var jsApp = {
-  onload: function() {
+import resources from './resources.js';
+import {PlayerEntity, EnemyEntity, CoinEntity, BoxEntity, BootsEntity} from './entities.js';
+import PlayScreen from './screens.js';
+import { DebugPanelPlugin } from "debugPlugin";
+export default function onload() {
+
+    // init the video
+    if (!me.video.init(800, 600, {parent : "screen", scaleMethod : "flex-width",  renderer : me.video.WEBGL, preferWebGL1 : false, depthTest: "z-buffer", subPixel : false})) {
+        alert("Your browser does not support HTML5 canvas.");
+        return;
+    }
+
+    // register the debug plugin
+    me.plugin.register(DebugPanelPlugin,  "debugPanel");
+
+    // allow cross-origin for image/texture loading
+	me.loader.setOptions({ crossOrigin : "anonymous" });
+
+    // set all ressources to be loaded
+    me.loader.preload(resources, () => {
+
+        // set the "Play/Ingame" Screen Object
+        me.state.set(me.state.PLAY, new PlayScreen());
+
+        // set the fade transition effect
+        me.state.transition("fade", "#FFFFFF", 250);
+
+        // register our objects entity in the object pool
+        me.pool.register("player", PlayerEntity);
+        me.pool.register("EnemyEntity", EnemyEntity);
+        me.pool.register("box", BoxEntity);
+        me.pool.register("coin", CoinEntity, true);
+        me.pool.register("boots",BootsEntity);
+
+      // commenting out texture; do we not have in this game, but I don't know that this setup can work without one. We'll see!
+        // load the texture atlas file
+        // this will be used by renderable object later
+//        game.texture = new me.TextureAtlas(
+//            me.loader.getJSON("texture"),
+//            me.loader.getImage("texture")
+//        );
+
+        // switch to PLAY state
+        me.state.change(me.state.PLAY);
+    });
+}
+/* Commenting out original main function
+export default function onload() {
     if (!me.video.init('jsapp', 320, 240, true, 2.0)) {
       alert("html 5 canvas is not supported by this browser.");
       return;
@@ -27,3 +73,4 @@ var jsApp = {
 window.onReady(function() {
   jsApp.onload();
 });
+*/
