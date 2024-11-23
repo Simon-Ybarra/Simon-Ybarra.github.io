52753d6d94aa055bf6e7d405976a14d1f8cbe886
Simon-Ybarra <sybarra6@calstatela.edu>
2024-11-23 12:13:20 -0800

    Add files via upload
    Adding initial draft of Hoppy Hunting project.

---
+++ Hoppy Hunting/resources.js
@@ -0,0 +1,56 @@
+var resources = [
+
+    /* Graphics.
+     */
+    { name: "MapTiles",         type:"image",   src: "MapTiles.png" },
+    { name: "Bunny",      type:"image",   src: "Bunny.png" },
+    { name: "Carrot",          type:"image",   src: "Carrot.png" },
+
+    /* Maps.
+     */
+    { name: "level1",            type: "tmx",    src: "level1.tmx" },
+
+
+    /* Tilesets.
+     */
+    { name: "MapTiles",         type: "tsx",    src: "MapTiles.tsx" },
+    { name: "Bunny",         type: "tsx",    src: "Bunny.tsx" },
+    { name: "Carrot",         type: "tsx",    src: "Carrot.tsx" }
+
+
+    /* Background music.
+     * @example
+     * { name: "example_bgm", type: "audio", src: "data/bgm/" },
+     */
+//    { name: "dst-gameforest",  type: "audio", src: "data/bgm/" },
+
+    /* Sound effects.
+     * @example
+     * { name: "example_sfx", type: "audio", src: "data/sfx/" }
+     */
+/*    { name: "cling",           type: "audio",  src: "data/sfx/" },
+    { name: "die",             type: "audio",  src: "data/sfx/" },
+    { name: "enemykill",       type: "audio",  src: "data/sfx/" },
+    { name: "jump",            type: "audio",  src: "data/sfx/" },
+*/
+
+    /* Atlases
+     * @example
+     * { name: "example_tps", type: "json", src: "data/img/example_tps.json" },
+     */
+    // texturePacker
+    /*
+    { name: "texture",         type: "json",   src: "data/img/texture.json" },
+    { name: "texture",         type: "image",  src: "data/img/texture.png" },
+*/
+    /* Bitmap Font
+    * @example
+    * { name: "example_fnt", type: "image", src: "data/img/example_fnt.png" },
+    * { name: "example_fnt", type: "binary", src: "data/img/example_fnt.fnt" },
+    
+    { name: "PressStart2P", type:"image", src: "data/fnt/PressStart2P.png" },
+    { name: "PressStart2P", type:"binary", src: "data/fnt/PressStart2P.fnt"}
+    */
+];
+
+export default resources;
